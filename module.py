9356799221bf1f5f7bb2769e4549297e21a20276
Eric Julianto <86970816+algonacci@users.noreply.github.com>
2023-10-05 13:20:25 +0700

    prompt tuning

--- module.py
+++ module.py
@@ -9,10 +9,9 @@ def generate(prompt):
     response = openai.Completion.create(
         engine="gpt-3.5-turbo-instruct",
         prompt="""
-        Buatkan sebuah materi untuk konten Instagram yang singkat, padat, dan jelas tentang {}
-        dalam format HTML dalam bentuk list.
-        Untuk setiap sub-judul dan list ditebalkan menggunakan bold,
-        setiap sub-judul diberikan baris baru menggunakan <br> dia memanjang ke bawah.
+        Buatkan sebuah materi untuk konten Instagram yang singkat tentang {}
+        dalam format list HTML.
+        Setiap sub-judul dan list ditebalkan menggunakan bold, serta diberikan baris baru menggunakan <br> supaya memanjang ke bawah.
         Berikan juga caption di akhir dengan 20 hashtags
         """.format(prompt),
         temperature=0.7,
